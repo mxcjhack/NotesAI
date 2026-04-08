0601976a9ea8437428a4e560787db01e1fc17f32
Prasoon Modi <prasoongreat.mvp@gmail.com>
2026-04-08 17:42:42 +0530

    Initializing project

---
+++ project/src/test/java/com/notesai/backend/project/TestProjectApplication.java
@@ -0,0 +1,11 @@
+package com.notesai.backend.project;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProjectApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
